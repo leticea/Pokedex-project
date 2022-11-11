de4e543170e24b5128d309d81acbf27964a2b525
leticea <nascimento.leticeah@gmail.com>
2022-11-11 18:25:30 -0300

    refatorando o código

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
+
 const fetchPokemon = () => {
 
-    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const generatePokemonPromises = () => Array(150).fill().map((_, index) =>
+        fetch(getPokemonUrl(index + 1)).then(response => response.json()))
+    
+    const pokemonPromises = generatePokemonPromises()
 
-    const pokemonPromises = [];
-
-    for (let i = 1; i <= 150; i++) {
-
-        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()));
-    }
-
-    // [All - método estático]
+    // [ALL - método estático]
     Promise.all(pokemonPromises)
         .then(pokemons => {
 
@@ -19,8 +17,8 @@ const fetchPokemon = () => {
                 const types = pokemon.types.map(typeInfo => typeInfo.type.name);
 
                 accumulator += `
-                    <li class="card">
-                    <img class="card-image ${types[0]}" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" />
+                    <li class="card ${types[0]}">
+                    <img class="card-image" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" />
                         <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
                         <p class="card-subtitle">${types.join(' | ')}</p>
                     </li>`
